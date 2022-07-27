4d8847ed42785186f166faf4e27bf3a404e86b90
dydwn6565 <55012173+dydwn6565@users.noreply.github.com>
2022-07-27 15:19:16 -0700

    finished explore page

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -1,9 +1,25 @@
 import React from 'react'
-
+import Header from './Header'
+import "./css/Explore.css"
 function Explore() {
   return (
-    <div>Explore</div>
-  )
+    <div >
+      <Header />
+      <div className="explore-container">
+        <div className="explore-images">
+
+        <img src="https://img.insight.co.kr/static/2022/05/05/700/img_20220505143127_nqoog77t.webp" />
+        <img src="https://img.insight.co.kr/static/2022/05/05/700/img_20220505143127_nqoog77t.webp" />
+        <img src="https://img.insight.co.kr/static/2022/05/05/700/img_20220505143127_nqoog77t.webp" />
+        <img src="https://img.insight.co.kr/static/2022/05/05/700/img_20220505143127_nqoog77t.webp" />
+        <img src="https://img.insight.co.kr/static/2022/05/05/700/img_20220505143127_nqoog77t.webp" />
+        <img src="https://img.insight.co.kr/static/2022/05/05/700/img_20220505143127_nqoog77t.webp" />
+        <img src="https://img.insight.co.kr/static/2022/05/05/700/img_20220505143127_nqoog77t.webp" />
+        <img src="https://img.insight.co.kr/static/2022/05/05/700/img_20220505143127_nqoog77t.webp" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Explore
