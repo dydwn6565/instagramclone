0d3512f45276aa5f854f5502f65dfcbf5f71db65
dydwn6565 <55012173+dydwn6565@users.noreply.github.com>
2022-08-13 02:10:13 -0700

    successfully completed new message (create chat groups)

--- src/components/UI/MyMessageModal.js
+++ src/components/UI/MyMessageModal.js
@@ -1,13 +1,19 @@
 import { Avatar, Button } from "@mui/material";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { useEffect } from "react";
 import Card from "./Card";
 import "./MyMessageModal.css";
 import { v4 as uuidv4 } from "uuid";
-import { Link } from "react-router-dom";
+import { MdClose } from "react-icons/md";
+
 function MyMessageModal({ messageModalHandler }) {
   const [userList, setUserList] = useState("");
-  const [filteredUserList,setFilteredUserList] = useState("");
+  const [filteredUserList, setFilteredUserList] = useState("");
+  const [clickedUserList, setClickedUserList] = useState([]);
+  const [filteredUserListShow, setfiltereUserListShow] = useState(false);
+  const [searchedUser, setSearchedUser] = useState("");
+
+  const ref = useRef(null);
   useEffect(() => {
     const getUserList = async () => {
       const userListData = await fetch("http://localhost:8080/users", {
@@ -17,37 +23,90 @@ function MyMessageModal({ messageModalHandler }) {
           "Access-Control-Allow-Origin": "*",
         },
       });
-      
+
       if (userListData.status === 201) {
         const userListJson = await userListData.json();
         setUserList(userListJson);
-        
+        userList && addCheckedInAPI(userList);
       }
     };
+
+    const addCheckedInAPI = (userList) => {
+      console.log(userList);
+      const addedChecked = userList.users.map((user) => {
+        return { ...user, checked: false };
+      });
+      setFilteredUserList(addedChecked);
+    };
+
     getUserList();
-  },[]);
+  }, []);
 
-  const userSearchHandler =(targetValue) =>{
-      console.log(targetValue)
+  const userSearchHandler = (targetValue) => {
+    console.log(targetValue);
+    console.log(ref.current.value);
 
-      if(targetValue !==""){
-        const filteredUserLIst = userList.users.filter((user)=>{
-          return user.userid.includes(targetValue)
-        })
-        setFilteredUserList(filteredUserLIst);
-        console.log(filteredUserList)
-      }else{
-        setFilteredUserList("")
-      }
-  }
+    if (targetValue !== "") {
+      const filteredUserLIst = userList.users.filter((user) => {
+        return user.userid.includes(targetValue);
+      });
+      // addCheckedInAPI(filteredUserLIst)
 
-  const createChat = () =>{
+      setSearchedUser(targetValue);
+      setFilteredUserList(filteredUserLIst);
+      setfiltereUserListShow(true);
+    } else {
+      setFilteredUserList("");
+      ref.current.value ="";
+    }
+  };
+
+  const createChat = () => {
     const randomRoomNumber = uuidv4();
-    
-    window.location.href=`/myMessage/${randomRoomNumber}`
-    
-  }
 
+    window.location.href = `/myMessage/${randomRoomNumber}`;
+  };
+
+
+
+  const radioButtonOnchangeHandler = (e) => {
+    setClickedUserList((users) => [...users, e.target.value]);
+    const updatedFilteredList = filteredUserList.filter((user) => {
+      if (user.userid === e.target.value) {
+        user.checked = !user.checked
+        return user;
+      } else {
+        return user;
+      }
+    });
+
+    setFilteredUserList(updatedFilteredList);
+    setfiltereUserListShow(false);
+    ref.current.value ="";
+    // setSearchedUser("");
+    // console.log(e.target.value)
+  };
+
+  const deleteReceiver = (id) => {
+    console.log(id);
+    console.log(clickedUserList);
+    const deletedOneUser = clickedUserList.filter((user) => {
+      return user !== id;
+    });
+
+    setClickedUserList(deletedOneUser);
+
+    const updatedFilteredList = filteredUserList.filter((user) => {
+      if (user.userid === id) {
+        user.checked = !user.checked;
+        return user;
+      } else {
+        return user;
+      }
+    });
+    console.log(updatedFilteredList);
+    setFilteredUserList(updatedFilteredList);
+  };
 
   return (
     <>
@@ -59,33 +118,58 @@ function MyMessageModal({ messageModalHandler }) {
         <header className="my-message-modal-header">
           <h2>New message</h2>
 
-          {/* <Link to="/"></Link> */}
           <span onClick={createChat}>Create</span>
         </header>
-          <div className="hr"></div>
+        <div className="hr"></div>
         {/* <FaPhotoVideo className="modal-photo-video" /> */}
         <div className="my-message-modal-content">
           <p>
             <strong> Receiver:</strong>
           </p>
+          <div className="my-message-receiver">
+            {clickedUserList.map((clickedUser) => (
+              <>
+                <div className="my-message-receiver-name">
+                  <div>{clickedUser}</div>
+                  <MdClose
+                    className="my-message-receiver-close"
+                    id={clickedUser}
+                    onClick={(e) => deleteReceiver(e.target.id)}
+                  />
+                </div>
+              </>
+            ))}
+          </div>
           <input
             type="text"
             placeholder="search"
+            ref={ref}
+            
             onChange={(e) => userSearchHandler(e.target.value)}
           />
         </div>
         <div className="hr"></div>
 
         <div className="my-message-search-container">
+          {/* {console.log(clickedUserList)} */}
           {filteredUserList &&
+            filteredUserListShow &&
             filteredUserList.map((user) => (
-              <div className="my-message-user-info-container">
+              <div className="my-message-user-info-container" key={user.userid}>
                 <Avatar></Avatar>
                 <div className="my-message-user-info-id">
                   <div>{user.userid}</div>
                   <div>{user.username}</div>
                 </div>
-                <input type="radio" className="my-message-recommend-id-radio" />
+                <input
+                  type="radio"
+                  className="my-message-recommend-id-radio"
+                  value={user.userid}
+                  checked={user.checked}
+                  readOnly
+                  // onClick={(e) => radioButtonHandler(e)}
+                  onClick={(e) => radioButtonOnchangeHandler(e)}
+                />
               </div>
             ))}
         </div>
@@ -97,7 +181,11 @@ function MyMessageModal({ messageModalHandler }) {
               <div>dydwn6565</div>
               <div>Yongju Lee</div>
             </div>
-            <input type="radio" className="my-message-recommend-id-radio" />
+            <input
+              type="radio"
+              className="my-message-recommend-id-radio"
+              // onChange={(e) => radioButtonHandler(e.target.value)}
+            />
           </div>
         </div>
       </Card>
